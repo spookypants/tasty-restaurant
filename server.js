a4e6a6228d2a94910907cf8c1b8c7848c999bc38
Gabe <42557432+spookypants@users.noreply.github.com>
2019-07-03 12:00:30 -0500

    initial files

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
 
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
